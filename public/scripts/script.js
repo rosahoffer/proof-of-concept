22b10baf7be93e85983811ebd73dd78506b1adaa
rosahoffer <rosahoffer@live.nl>
2023-06-20 12:06:31 +0200

    commits by autocomplete script

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,22 +1,30 @@
-
-// A collection of documents for our examples
+// Console.log de array met alle titels
 console.log(allTitles);
- 
-const input = document.getElementById('search-term')
-const autocompleteResults = document.querySelector('.autocomplete-options')
 
+// Zoek het input-element en het element voor de autocomplete-resultaten op
+const input = document.getElementById('search-term');
+const autocompleteResults = document.querySelector('.autocomplete-options');
+
+// Controleer of het input-element bestaat
 if (input) {
+    // Luister naar het 'input' evenement op het input-element
     input.addEventListener('input', (event) => {
-        autocompleteResults.innerHTML = ''
-        if (input.value.length ) {
+        // Leeg de HTML van de autocomplete-resultaten
+        autocompleteResults.innerHTML = '';
+
+        // Controleer of de lengte van de ingevoerde waarde groter is dan 0
+        if (input.value.length) {
+            // Doorloop alle titels
             allTitles.forEach(title => {
-            const lowerCaseTitle = title.toLowerCase()
+                // Converteer de titel naar kleine letters
+                const lowerCaseTitle = title.toLowerCase();
+
+                // Controleer of de ingevoerde waarde voorkomt in de titel
                 if (lowerCaseTitle.includes(input.value.toLowerCase())) {
-                    autocompleteResults.innerHTML += `<li>${title}</li>`
+                    // Voeg een lijstitem toe aan de autocomplete-resultaten
+                    autocompleteResults.innerHTML += `<li>${title}</li>`;
                 }
-            })
+            });
         }
-        // myArray.filter(element => element.includes("substring"));
-        // const foundPosts =  getResults(allTitles, input.value)
-    })
+    });
 }
