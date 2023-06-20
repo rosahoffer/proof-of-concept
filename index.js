2fb72af823ee00741fa23a05f0ef2375df121995
rosahoffer <rosahoffer@live.nl>
2023-06-20 11:27:55 +0200

    final autocomplete

--- index.js
+++ index.js
@@ -78,69 +78,8 @@ app.get('/', async function (req, res) {
   });
 
   const data = await response.json();
-  console.log(data);
-
-  res.render('index', data);
-});
-
-// Route voor de autocomplete
-app.get('/autocomplete', async function (req, res) {
-  const searchTerm = req.query.searchTerm || '';
-
-  // Functie om de autocomplete-resultaten op te halen
-  async function getAutocompleteResults(searchTerm) {
-    try {
-      const apiUrl = 'https://graphql.datocms.com/';
-      const headers = {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer 10a0ae10c2d6418c1acd4346de9329',
-      };
-
-      const params = {
-        searchTerm: searchTerm,
-      };
-
-      // Doe de API-aanroep met fetch
-      const response = await fetch(`${apiUrl}/autocomplete?searchTerm=${searchTerm}`, {
-        method: 'GET',
-        headers: headers,
-      });
-
-      // Controleer of de API-aanroep succesvol was
-      if (!response.ok) {
-        throw new Error('API call failed');
-      }
-
-      // Lees de JSON-respons van de API
-      const data = await response.json();
-
-      // Verwerk de ontvangen data en retourneer de resultaten
-      const processAutocompleteData = (data) => {
-        try {
-          // Controleer of de ontvangen data een array is
-          if (Array.isArray(data)) {
-            return data;
-          } else {
-            console.error('Ontvangen data is geen array:', data);
-            return [];
-          }
-        } catch (handleError) {
-          console.error('Error retrieving autocomplete results:', handleError);
-          return [];
-        }
-      };
-
-      const autocompleteResults = processAutocompleteData(data);
-      return autocompleteResults;
-    } catch (error) {
-      console.error('Error retrieving autocomplete results:', error);
-      return [];
-    }
-  }
-
-  const autocompleteResults = await getAutocompleteResults(searchTerm);
-  const resultsArray = Array.isArray(autocompleteResults) ? autocompleteResults : [];
-  res.json(resultsArray);
+  const allTitles = data.data.allBlogPosts.map(post => post.title)
+  res.render('index', {data, allTitles});
 });
 
 // Stel het poortnummer in waar express op gaat luisteren
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,40 +1,22 @@
-$(document).ready(function() {
-    // Referentie naar het datalist-element
-    var dataList = $('#title-list');
-  
-    // Luister naar keyup-event op het zoekveld
-    $('#search-term').keyup(function() {
-      var searchTerm = $(this).val();
-  
-      // Doe een API-aanroep om de autocomplete-resultaten op te halen
-      $.get('/autocomplete', { searchTerm: searchTerm }, function(data) {
-        console.log(data);
-        // Wis de vorige resultaten
-        dataList.empty();
-  
-        // Verwerk de ontvangen resultaten
-        data.forEach(function(result) {
-          // Voeg een option-element toe aan het datalist-element voor elk resultaat
-          var optionElement = $('<option></option>');
-  
-          // Stel de waarde in op de titel van het resultaat
-          optionElement.val(result.title);
-  
-          // Voeg het option-element toe aan het datalist-element
-          dataList.append(optionElement);
-        });
-  
-        // Console log: de ontvangen resultaten
-        console.log('Ontvangen resultaten:', data);
-      })
-      .done(function() {
-        // Console log: API-aanroep succesvol afgerond
-        console.log('API-aanroep succesvol afgerond');
-      })
-      .fail(function() {
-        // Console log: API-aanroep mislukt
-        console.log('API-aanroep mislukt');
-      });
-    });
-  });
-  
+
+// A collection of documents for our examples
+console.log(allTitles);
+ 
+const input = document.getElementById('search-term')
+const autocompleteResults = document.querySelector('.autocomplete-options')
+
+if (input) {
+    input.addEventListener('input', (event) => {
+        autocompleteResults.innerHTML = ''
+        if (input.value.length ) {
+            allTitles.forEach(title => {
+            const lowerCaseTitle = title.toLowerCase()
+                if (lowerCaseTitle.includes(input.value.toLowerCase())) {
+                    autocompleteResults.innerHTML += `<li>${title}</li>`
+                }
+            })
+        }
+        // myArray.filter(element => element.includes("substring"));
+        // const foundPosts =  getResults(allTitles, input.value)
+    })
+}
